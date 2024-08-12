16afd75d7f8c975559875fe0c671d2299a8e467c
readboy8 <141379490+readboy8@users.noreply.github.com>
2024-08-12 10:54:27 -0500

    Implement saving and loading function

--- main.py
+++ main.py
@@ -1,7 +1,56 @@
 import tkinter as tk
 import json
 from tkinter import ttk
-import re
+import pickle
+
+
+def latest_run_money():
+    moon = set_moon_field.get()
+    if moon == '':
+        print('Invalid Moon')
+    else:
+        credits = set_money_field.get()
+        totalCredits[moon] += int(credits)
+        print(totalCredits)
+        set_money_field.delete(0, 'end')
+
+
+def save():
+    #save_json = json.dumps(totalCredits, indent=4)
+    with open("./saves/totalCredits.lethalStat", "wb") as outfile:
+        pickle.dump(totalCredits, outfile)
+
+
+def load():
+    loaded_object = {'Experimentation': 0, 'Assurance': 0, 'Vow': 0, 'Offense': 0, 'March': 0, 'Adamance': 0, 'Rend': 0,
+                     'Dine': 0, 'Titan': 0, 'Artifice': 0, 'Embrion': 0}
+
+    with open("./saves/totalCredits.lethalstat", "rb") as openfile:
+        loaded_object = pickle.load(openfile)
+    exp = loaded_object['Experimentation']
+    ass = loaded_object['Assurance']
+    vow = loaded_object['Vow']
+    off = loaded_object['Offense']
+    mar = loaded_object['March']
+    ada = loaded_object['Adamance']
+    ren = loaded_object['Rend']
+    din = loaded_object['Dine']
+    tit = loaded_object['Titan']
+    art = loaded_object['Artifice']
+    emb = loaded_object['Embrion']
+
+    totalCredits['Experimentation'] = exp
+    totalCredits['Assurance'] = ass
+    totalCredits['Vow'] = vow
+    totalCredits['Offense'] = off
+    totalCredits['March'] = mar
+    totalCredits['Adamance'] = ada
+    totalCredits['Rend'] = ren
+    totalCredits['Dine'] = din
+    totalCredits['Titan'] = tit
+    totalCredits['Artifice'] = art
+    totalCredits['Embrion'] = emb
+
 
 totalCredits = {
     'Experimentation': 0,
@@ -16,17 +65,6 @@ totalCredits = {
     'Artifice': 0,
     'Embrion': 0
 }
-
-def latest_run_money():
-    moon = set_moon_field.get()
-    if moon == '':
-        print('Invalid Moon')
-    else:
-        credits = set_money_field.get()
-        totalCredits[moon] += int(credits)
-        print(totalCredits)
-        set_money_field.delete(0, 'end')
-
 
 root = tk.Tk()
 
@@ -60,5 +98,13 @@ set_money_field = ttk.Entry(mainframe)
 set_money_field.grid(row=3, column=0, pady=10, sticky='NWES')
 set_latest_credits_button = ttk.Button(mainframe, text='Confirm', command=latest_run_money)
 set_latest_credits_button.grid(row=3, column=1, pady=10)
+
+# JSON save button
+save_button = ttk.Button(mainframe, text='Save', command=save)
+save_button.grid(row=0, column=3, pady=10, sticky='NWES')
+
+# JSON load button
+load_button = ttk.Button(mainframe, text='Load', command=load)
+load_button.grid(row=0, column=4, pady=10, sticky='NWES')
 
 root.mainloop()
